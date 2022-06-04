9792a0634f6496d9fae3ad5e5e9cbf076326b7fa
monjuAflame <monjuaflame@gmail.com>
2022-06-05 00:22:02 +0600

    realtion data upload methods two

--- app/Imports/CustomerImportRelationship.php
+++ app/Imports/CustomerImportRelationship.php
@@ -10,10 +10,21 @@ use Maatwebsite\Excel\Concerns\ToModel;
 
 class CustomerImportRelationship implements ToModel
 {
+
+    private $customers;
+
+    public function __construct()
+    {
+        $this->customers = Customer::select('id', 'first_name', 'last_name')->get()
+                                    ->key(function($item) {
+                                        return $item->first_name . ' ' . $item->last_name;
+                                    })->toArray();
+    }
+
     public function model(array $row)
     {
         return new Purchase([
-            'customer_id' => $this->getCustomerIdDB($row[1], $row[2]),
+            'customer_id' => $this->getCustomerId($row[1], $row[2]),
             'acc_no' => $row[3],
             'company' => $row[4]
         ]);
@@ -22,6 +33,14 @@ class CustomerImportRelationship implements ToModel
     public function getCustomerIdDB($first_name, $last_name)
     {
         $customer = Customer::where('first_name', $first_name)->where('last_name', $last_name)->first();
+        if($customer) return null;
+
+        return $customer->id;
+    }
+
+    public function getCustomerId($first_name, $last_name)
+    {
+        $customer = $this->customers[$first_name . ' ' . $last_name] ?? null;
         if($customer) return null;
 
         return $customer->id;
